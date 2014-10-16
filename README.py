e86ba24f6e6ea6e07ad2a45ee9604a7809efe74c
cherianyit <cherianyit9@gmail.com>
2014-10-15 20:19:40 -0400

    Update README.py

--- README.py
+++ README.py
@@ -24,3 +24,25 @@ Illegal domain error
 -4
 -2
 3
+#Exercise 1
+#Volume and Surface Area of a Sphere
+from math import *
+def volSurfSphere():
+    radi=eval(input("What is the radius? "))
+    vol=(4/3)*pi*radi**3
+    area=4*pi*radi**2
+    print ("Volume = ",vol)
+    print ("Surface Area = ",area)
+#Exercise 5
+#Pound Thing
+def pound():
+    pound=eval(input("How many pounds are being ordered? "))
+    cost=pound*10.50+pound*0.86+1.50
+    print("The cost is: ",cost)
+#Exercise 6
+#Slope Formula
+def slope():
+    x1,y1=eval(input("Type in the x1 and y1 coordinates seperated by a comma: "))
+    x2,y2=eval(input("Type in the x2 and y2 coordinates seperated by a comma: "))
+    slope=(y2-y1)/(x2-x1)
+    print("The slope is: ",slope)
